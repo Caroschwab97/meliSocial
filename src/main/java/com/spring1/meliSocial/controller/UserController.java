02c1377980d4317b358cbb23276a0402730e12c8
nuriarobledo <nuriarobledo0@gmail.com>
2024-12-12 23:08:46 -0300

    feat: agrego endpoints de getAllUsers y follow a un vendedor

--- src/main/java/com/spring1/meliSocial/controller/UserController.java
+++ src/main/java/com/spring1/meliSocial/controller/UserController.java
@@ -2,7 +2,13 @@ package com.spring1.meliSocial.controller;
 
 import com.spring1.meliSocial.service.IPostService;
 import com.spring1.meliSocial.service.IUserService;
+import com.spring1.meliSocial.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +16,14 @@ public class UserController {
 
     @Autowired
     private IUserService service;
+
+    @GetMapping("users")
+    public ResponseEntity<?> getUsers() {
+        return ResponseEntity.ok(service.getUsers());
+    }
+
+    @PostMapping("/users/{userId}/follow/{userIdToFollow}")
+    public ResponseEntity<?> followUser(@PathVariable int userId, @PathVariable int userIdToFollow){
+        return ResponseEntity.ok(service.followUser(userId, userIdToFollow));
+    }
 }
