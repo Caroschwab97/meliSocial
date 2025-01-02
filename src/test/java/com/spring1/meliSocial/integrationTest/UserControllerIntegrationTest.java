7d713dbc06fdcceac44a84cb00a966d8f616eb57
camilopez2001 <camilopez8501@gmail.com>
2025-01-02 16:42:13 -0300

    feat: testing de seguir a un usuario y sus excepciones

--- src/test/java/com/spring1/meliSocial/integrationTest/UserControllerIntegrationTest.java
+++ src/test/java/com/spring1/meliSocial/integrationTest/UserControllerIntegrationTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import org.springframework.test.web.servlet.ResultMatcher;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,13 +39,26 @@ public class UserControllerIntegrationTest {
         int parametroEntrada = 1;
         UserFollowersDto result = new UserFollowersDto(1, "Agustina Lopez", 4);
 
-        ResultMatcher statusEsperado = status().isOk();
-        ResultMatcher contentTypeEsperado = content().contentType("application/json");
-        ResultMatcher bodyEsperado = content().json(objectMapper.writeValueAsString(result));
+        mockMvc.perform(get("/users/{userId}/followers/count", parametroEntrada))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
+                )
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Obtener NotFound de la cantidad de usuarios que siguen a un determinado vendedor")
+    public void testGetFollowerCountNotFound() throws Exception {
+        int parametroEntrada = 10;
+        ResponseDto result = new ResponseDto("El id que busca no existe");
 
         mockMvc.perform(get("/users/{userId}/followers/count", parametroEntrada))
                 .andExpectAll(
-                        statusEsperado, contentTypeEsperado, bodyEsperado
+                        status().isNotFound(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
                 )
                 .andDo(print());
     }
@@ -58,38 +70,110 @@ public class UserControllerIntegrationTest {
         int user = 4;
         int userToFollow = 1;
 
-
-        ResultMatcher statusEsperado = status().isOk();
-        ResultMatcher contentTypeEsperado = content().contentType("application/json");
-        ResultMatcher bodyEsperado = content().json(objectMapper.writeValueAsString(result));
-        {
-
-        }
         mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
                 .andExpectAll(
-                        statusEsperado, contentTypeEsperado, bodyEsperado
+                        status().isOk(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
                 )
                 .andDo(print());
     }
 
     @Test
-    @DisplayName("Obtener NotFound de la cantidad de usuarios que siguen a un determinado vendedor")
-    public void testGetFollowerCountNotFound() throws Exception {
-        int parametroEntrada = 10;
-        ResponseDto result = new ResponseDto("El id que busca no existe");
-        ResultMatcher statusEsperado = status().isNotFound();
-        ResultMatcher contentTypeEsperado = content().contentType("application/json");
-        ResultMatcher bodyEsperado = content().json(objectMapper.writeValueAsString(result));
+    @DisplayName("“Follow” (seguir) a un determinado vendedor NotFound user")
+    public void testFollowUserNotFoundUser() throws Exception {
+        int user = 40;
+        int userToFollow = 1;
+        ResponseDto result = new ResponseDto("El usuario con ID: " + user + " no existe.");
 
-        mockMvc.perform(get("/users/{userId}/followers/count", parametroEntrada))
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
                 .andExpectAll(
-                        statusEsperado, contentTypeEsperado, bodyEsperado
+                        status().isNotFound(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
+                )
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("“Follow” (seguir) a un determinado vendedor NotFound userToFollow")
+    public void testFollowUserNotFoundUserToFollow() throws Exception {
+        int user = 4;
+        int userToFollow = 100;
+        ResponseDto result = new ResponseDto("El usuario a seguir con ID: " + userToFollow + " no existe.");
+
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
+                )
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("“Follow” (seguir) a un determinado vendedor con ID iguales")
+    public void testFollowUserBadRequestExceptionEqualsID() throws Exception {
+        int user = 4;
+        int userToFollow = 4;
+        ResponseDto result = new ResponseDto("Un usuario no puede seguirse a sí mismo.");
+
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
+                )
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("“Follow” (seguir) a un determinado vendedor - ya contiene ese seguidor")
+    public void testFollowUserBadRequestExceptionRepeatFollower() throws Exception {
+        int user = 2;
+        int userToFollow = 1;
+        ResponseDto result = new ResponseDto("El usuario con ID: " + user + " ya sigue al usuario con ID: " + userToFollow);
+
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
                 )
                 .andDo(print());
     }
 
 
+    @Test
+    @DisplayName("“Follow” (seguir) a un determinado vendedor - el usuario a seguir no es vendedor")
+    public void testFollowUserBadRequestExceptionNoASeller() throws Exception {
+        int user = 2;
+        int userToFollow = 3;
+        ResponseDto result = new ResponseDto("Un comprador solo puede seguir a un usuario vendedor.");
 
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result))
+                )
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("“Follow” (seguir) a un determinado vendedor - el usuario a seguir no es vendedor")
+    public void testFollowUserBadRequestExceptionNoASeller2() throws Exception {
+        int user = 1;
+        int userToFollow = 2;
+        ResponseDto result = new ResponseDto("Solo se puede seguir a un usuario vendedor.");
+
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentType("application/json"),
+                        content().json(objectMapper.writeValueAsString(result)))
+                .andDo(print());
+    }
 
     @Test
     public void testFollowedList() throws Exception {
@@ -154,5 +238,4 @@ public class UserControllerIntegrationTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.followed[1].user_id").value(seguidos.get(1).getId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.followed[1].user_name").value(seguidos.get(1).getUserName()));
     }
-
 }
