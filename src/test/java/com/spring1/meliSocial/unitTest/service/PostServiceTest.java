2d0559f23d65ad4b8fa29378f393833458be6a4a
MarianoChun <marianochun01@gmail.com>
2025-01-02 11:50:31 -0300

    add test posts de mas de dos semanas y fix suite test post service

--- src/test/java/com/spring1/meliSocial/unitTest/service/PostServiceTest.java
+++ src/test/java/com/spring1/meliSocial/unitTest/service/PostServiceTest.java
@@ -42,12 +42,15 @@ public class PostServiceTest {
     PostService postService;
 
     User seller1;
-    User user1;
+    User user2;
     User user9;
     Post post1;
     Post post2;
     Post post17;
     Post post18;
+    Post post19;
+    Post oldPost200;
+    Post oldPost300;
     List<Post> postsEsperados;
     ResponsePostDto postDto17;
     ResponsePostDto postDto18;
@@ -55,19 +58,23 @@ public class PostServiceTest {
     @BeforeEach
     public void setUp() {
         seller1 = new User(1, "seller1", true, List.of(2), new ArrayList<>(), new ArrayList<>(), new HashSet<>());
-        user1 = new User(2, "user1", false, new ArrayList<>(), List.of(1), new ArrayList<>(), new HashSet<>());
+        user2 = new User(2, "user 2", false, new ArrayList<>(), List.of(1), new ArrayList<>(), new HashSet<>());
         post1 = new Post(1, 1, LocalDate.now(), null, 2, 100.0, false, 0.0);
         post2 = new Post(2, 1, LocalDate.now(), null, 2, 250.0, false, 0.0);
 
         //test de post x order
         user9= new User(9, "Lucia Quezada", false, List.of(), List.of(8), List.of(), Set.of());
-        post17 = new Post(17, 8, LocalDate.parse("2025-01-01"), new Product(), 2, 20.99, true, 0.3);
-        post18 = new Post(18, 8, LocalDate.parse("2025-01-02"), new Product(), 55, 15.99, true, 0.5
-        );
+        post17 = new Post(17, 8, LocalDate.now().minusDays(2), new Product(), 2, 20.99, true, 0.3);
+        post18 = new Post(18, 8, LocalDate.now().minusDays(1), new Product(), 55, 15.99, true, 0.5);
+        post19 = new Post(19, 1, LocalDate.now(), new Product(), 55, 25.99, true, 0.5);
         postsEsperados = List.of(post17, post18);
-        postDto17 = new ResponsePostDto(17, 8, LocalDate.parse("2025-01-01"), null, 2, 20.99, true, 0.3);
-        postDto18 = new ResponsePostDto(18, 8, LocalDate.parse("2025-01-02"), null, 55, 15.99, true, 0.5);
+        postDto17 = new ResponsePostDto(17, 8, LocalDate.now().minusDays(2), null, 2, 20.99, true, 0.3);
+        postDto18 = new ResponsePostDto(18, 8, LocalDate.now().minusDays(1), null, 55, 15.99, true, 0.5);
+        oldPost200 = new Post(200, 1, LocalDate.now().minusDays(15), new Product(), 2, 22.00, false, 0.0);
+        oldPost300 = new Post(300, 1, LocalDate.now().minusDays(15), new Product(), 2, 55.00, false, 0.0);
+
     }
+
 
     @Test
     @DisplayName("6- Verificar el correcto ordenamiento ascendente")
@@ -88,8 +95,8 @@ public class PostServiceTest {
         assertEquals(2, posteos.size());
         assertEquals(17, posteos.get(0).getId());
         assertEquals(18, posteos.get(1).getId());
-        assertEquals(LocalDate.parse("2025-01-01"), posteos.get(0).getDate());
-        assertEquals(LocalDate.parse("2025-01-02"), posteos.get(1).getDate());
+        assertEquals(LocalDate.now().minusDays(2), posteos.get(0).getDate());
+        assertEquals(LocalDate.now().minusDays(1), posteos.get(1).getDate());
     }
 
     @Test
@@ -111,13 +118,13 @@ public class PostServiceTest {
         assertEquals(2, posteos.size());
         assertEquals(18, posteos.get(0).getId());
         assertEquals(17, posteos.get(1).getId());
-        assertEquals(LocalDate.parse("2025-01-02"), posteos.get(0).getDate());
-        assertEquals(LocalDate.parse("2025-01-01"), posteos.get(1).getDate());
+        assertEquals(LocalDate.now().minusDays(1), posteos.get(0).getDate());
+        assertEquals(LocalDate.now().minusDays(2), posteos.get(1).getDate());
     }
 
     @Test
     public void testGetPostsByUser_SortedByDateDescending() {
-        when(userRepository.getUserById(2)).thenReturn(Optional.of(user1));
+        when(userRepository.getUserById(user2.getId())).thenReturn(Optional.of(user2));
         when(postRepository.getPosts()).thenReturn(List.of(post1, post2));
 
         when(customMapper.mapToResponsePostDto(post1)).thenReturn(new ResponsePostDto(
@@ -170,7 +177,7 @@ public class PostServiceTest {
 
     @Test
     public void testGetPostsByUser_UserIdNoResult() {
-        when(userRepository.getUserById(1)).thenReturn(Optional.of(seller1));
+        when(userRepository.getUserById(seller1.getId())).thenReturn(Optional.of(seller1));
         when(postRepository.getPosts()).thenReturn(List.of(post1, post2));
 
         PostIndexDto result = postService.getPostsByUser(1, "date_desc");
@@ -179,5 +186,31 @@ public class PostServiceTest {
         List<ResponsePostDto> resultPosts = result.getPosts();
         assertEquals(1, resultUserId);
         assertEquals(0, resultPosts.size());
+    }
+
+    @Test
+    public void testGetPostsByUser_AllPostsWerePublishedMoreThanTwoWeeksAgo() {
+        when(userRepository.getUserById(user2.getId())).thenReturn(Optional.of(user2));
+        when(postRepository.getPosts()).thenReturn(List.of(oldPost200, oldPost300));
+
+        PostIndexDto result = postService.getPostsByUser(user2.getId(), "date_desc");
+
+        int resultUserId = result.getUserId();
+        List<ResponsePostDto> resultPosts = result.getPosts();
+        assertEquals(2, resultUserId);
+        assertEquals(0, resultPosts.size());
+    }
+
+    @Test
+    public void testGetPostsByUser_SomePostsWerePublishedMoreThanTwoWeeksAgo() {
+        when(userRepository.getUserById(user2.getId())).thenReturn(Optional.of(user2));
+        when(postRepository.getPosts()).thenReturn(List.of(post19,oldPost200));
+
+        PostIndexDto result = postService.getPostsByUser(user2.getId(), "date_desc");
+
+        int resultUserId = result.getUserId();
+        List<ResponsePostDto> resultPosts = result.getPosts();
+        assertEquals(2, resultUserId);
+        assertEquals(1, resultPosts.size());
     }
 }
