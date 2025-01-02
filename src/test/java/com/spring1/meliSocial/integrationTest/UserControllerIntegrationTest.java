83816c647ef04191dbdf20df8cbb6f5be1e81777
Caroschwab97 <caroschwab97@gmail.com>
2025-01-02 12:37:56 -0300

    fix: saco imports no usados

--- src/test/java/com/spring1/meliSocial/integrationTest/UserControllerIntegrationTest.java
+++ src/test/java/com/spring1/meliSocial/integrationTest/UserControllerIntegrationTest.java
@@ -3,27 +3,18 @@ package com.spring1.meliSocial.integrationTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring1.meliSocial.dto.response.FollowedByUserDto;
 import com.spring1.meliSocial.dto.response.FollowedDto;
-import com.spring1.meliSocial.dto.response.FollowerDto;
-import com.spring1.meliSocial.dto.response.SellerFollowedDto;
-import com.spring1.meliSocial.model.User;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.engine.TestExecutionResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
