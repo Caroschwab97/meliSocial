1c5c4b396b182af90eb54b29c8063657c33b8792
camilopez2001 <camilopez8501@gmail.com>
2025-01-02 12:23:22 -0300

    feat: test para follow user ok

--- src/test/java/com/spring1/meliSocial/integrationTest/UserControllerIntegrationTest.java
+++ src/test/java/com/spring1/meliSocial/integrationTest/UserControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.spring1.meliSocial.integrationTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring1.meliSocial.dto.response.ResponseDto;
 import com.spring1.meliSocial.dto.response.UserFollowersDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,7 +28,7 @@ public class UserControllerIntegrationTest {
 
     @Test
     @DisplayName("Obtener el resultado de la cantidad de usuarios que siguen a un determinado vendedor")
-    public void getFollowerCountIntegrationTest() throws Exception {
+    public void testGetFollowerCountOK() throws Exception {
         int parametroEntrada = 1;
         UserFollowersDto result = new UserFollowersDto(1, "Agustina Lopez", 4);
 
@@ -35,6 +37,27 @@ public class UserControllerIntegrationTest {
         ResultMatcher bodyEsperado = content().json(objectMapper.writeValueAsString(result));
 
         mockMvc.perform(get("/users/{userId}/followers/count", parametroEntrada))
+                .andExpectAll(
+                        statusEsperado, contentTypeEsperado, bodyEsperado
+                )
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Poder realizar la acción de “Follow” (seguir) a un determinado vendedor")
+    public void testFollowUserOK() throws Exception {
+        ResponseDto result = new ResponseDto("Siguiendo al usuario: Agustina Lopez con ID: 1");
+        int user = 4;
+        int userToFollow = 1;
+
+
+        ResultMatcher statusEsperado = status().isOk();
+        ResultMatcher contentTypeEsperado = content().contentType("application/json");
+        ResultMatcher bodyEsperado = content().json(objectMapper.writeValueAsString(result));
+        {
+
+        }
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",user,userToFollow))
                 .andExpectAll(
                         statusEsperado, contentTypeEsperado, bodyEsperado
                 )
